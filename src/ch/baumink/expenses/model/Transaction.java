be7de4f834e2b83871073f8cbcf738a9c77415e5
JonasGredig <jongredig@gmail.com>
2018-02-19 21:58:31 +0100

    added fields, getter and setter to Transaction

--- src/ch/baumink/expenses/model/Transaction.java
+++ src/ch/baumink/expenses/model/Transaction.java
@@ -1,4 +1,62 @@
 package ch.baumink.expenses.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Transaction {
+
+    private String name;
+    private ArrayList<String> tags;
+    private String category;
+    private Date date;
+    private int importance;
+    private String comment;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getImportance() {
+        return importance;
+    }
+
+    public void setImportance(int importance) {
+        this.importance = importance;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
