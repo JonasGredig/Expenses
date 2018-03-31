85b97d949663922b0717fff78e82ed401a9d8c59
JonasGredig <jongredig@gmail.com>
2018-03-31 02:09:58 +0200

    added LoginController with the methods login and register, but no implementation yet

---
+++ src/ch/baumink/expenses/controller/LoginController.java
@@ -0,0 +1,14 @@
+package ch.baumink.expenses.controller;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class LoginController {
+
+    public void login(String email, String password) {
+        throw new NotImplementedException();
+    }
+
+    public void register(String email, String password) {
+        throw new NotImplementedException();
+    }
+}
